e100a82bd7f3d1f0b8f79720dd98920214b3af41
alerodriguez01 <rodriguezalejandro_@hotmail.com>
2022-09-18 22:44:01 -0300

    - binding en todos los fragmentos y actividad - toolbar navegable entre fragmentos, con las 4 opciones: buscar, mis reservas, mis favoritos, configuracion.

--- app/src/main/java/com/mdgz/dam/labdam2022/DetalleAlojamientoFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/DetalleAlojamientoFragment.java
@@ -2,11 +2,16 @@ package com.mdgz.dam.labdam2022;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mdgz.dam.labdam2022.databinding.FragmentBusquedaBinding;
+import com.mdgz.dam.labdam2022.databinding.FragmentDetalleAlojamientoBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DetalleAlojamientoFragment extends Fragment {
+
+    protected FragmentDetalleAlojamientoBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +66,14 @@ public class DetalleAlojamientoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_alojamiento, container, false);
+        binding = FragmentDetalleAlojamientoBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+
     }
 }
--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,14 +1,72 @@
 package com.mdgz.dam.labdam2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mdgz.dam.labdam2022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbarPropia;
+        setSupportActionBar(toolbar);
+
+        //Toolbar "navegable" entre fragmentos
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                                                            .findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.buscar:
+                // User chose the "Settings" item, show the app settings UI...
+
+                return true;
+
+            case R.id.misReservas:
+
+                return true;
+
+            case R.id.misFavoritos:
+
+                return true;
+
+            case R.id.configuracion:
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
