e57cee49073f7048276c74a7a210b3b391d7f360
fedeTourn <fede450@outlook.com>
2022-10-14 13:24:52 -0300

    Agrega ToolBar personalizada y opciones Se agregaron los colores para el tema MyTheme, se creo el menú main_menu el cual tiene los items de las opciones que se piden, se agrego la logica de la toolbar y las opciones a MainActivity.

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,14 +1,28 @@
 package com.mdgz.dam.labdam2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar miToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        miToolbar = findViewById(R.id.mi_toolbar);
+        setSupportActionBar(miToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
