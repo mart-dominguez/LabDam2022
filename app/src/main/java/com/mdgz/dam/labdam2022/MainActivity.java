9a5e5b37e9df119228ee8448ad8a2d79dc80bbe8
fedeTourn <fede450@outlook.com>
2022-10-15 13:25:52 -0300

    Agrega interfaz del fragmento busqueda

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mdgz.dam.labdam2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +24,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mnuOptBuscar:
+                /* Agregar el intento del fragmento */
+                return true;
+            case R.id.mnuOptReservas:
+                /* Agregar el intento del fragmento */
+                return true;
+            case R.id.mnuOptFavoritos:
+                /* Agregar el intento del fragmento */
+                return true;
+            case R.id.mnuOptConfiguracion:
+                /* Agregar el intento del fragmento */
+                return true;
+            default:
+                Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
