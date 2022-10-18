6534a4fdca9b0b3727cc8cb61b5cd2b8cb30db97
Germán Vanni <germanvanni1@gmail.com>
2022-10-18 10:46:03 -0300

    navegación de toolbar

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,12 +1,12 @@
 package com.mdgz.dam.labdam2022;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -22,19 +22,53 @@ public class MainActivity extends AppCompatActivity {
         View root = binding.getRoot();
         setContentView(root);
 
-        setSupportActionBar(binding.materialToolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setTitle("Titulo toolbar");
+        setSupportActionBar(binding.toolbar);
+//        ActionBar ab = getSupportActionBar();
+////        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+////        ab.setDisplayHomeAsUpEnabled(true);
+//        ab.setTitle("Titulo toolbar");
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            binding.drawerLayout.openDrawer(GravityCompat.START);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch(item.getItemId()){
+            case R.id.nav_busqueda:
+                BusquedaFragment busquedaFragment = new BusquedaFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, busquedaFragment)
+                        .addToBackStack(null).commit();
+                break;
+            case R.id.nav_reservas:
+                MisReservasFragment misReservasFragment = new MisReservasFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, misReservasFragment)
+                        .addToBackStack(null).commit();
+                break;
+            case R.id.nav_favoritos:
+                MisFavoritosFragment misFavoritosFragment = new MisFavoritosFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, misFavoritosFragment)
+                        .addToBackStack(null).commit();
+                break;
+            case R.id.nav_configuracion:
+                ConfiguracionFragment configuracionFragment = new ConfiguracionFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, configuracionFragment)
+                        .addToBackStack(null).commit();
+                break;
+
+
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
     }
 }
