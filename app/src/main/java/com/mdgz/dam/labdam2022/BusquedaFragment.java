2951a620750244232d5297f103cb2f8ebf9e509d
fedeTourn <fede450@outlook.com>
2022-10-20 22:58:30 -0300

    Agrega logica boton busqueda(toolbar) y boton buscar(fragment Busqueda) No se resuelve la busqueda en la implementacion del boton buscar(enunciado). La navegacion entre fragmento busqueda y fragmento resultadosBusqueda y el boton de busqueda(toolbar) y el fragmento buscar se realiza usando navigation. La navegacion con el boton de busqueda(toolbar) se hace sin actions(confirmar si es la manera correcta)(agregar mensaje de excepcion). Se debe corregir la interfaz del fragmento de busqueda.

--- app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
@@ -1,64 +1,64 @@
 package com.mdgz.dam.labdam2022;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BusquedaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BusquedaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Spinner tipoAlojamiento, cantOcupantes, ciudad;
+    private CheckBox wifi;
+    private EditText precioMinimo, precioMaximo;
+    private Button btnBuscar, btnLimpiar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public interface OnBuscarListener{
+        public void buscar();
+    }
+    private OnBuscarListener listenerBuscar;
+
+    @Override
+    public void onAttach(@NonNull Context activity) {
+        super.onAttach(activity);
+        try {
+            listenerBuscar = (OnBuscarListener) activity;
+        }catch (ClassCastException e){
+            throw new ClassCastException(activity.toString() + "must implement interface");
+        }
+    }
 
     public BusquedaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BusquedaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BusquedaFragment newInstance(String param1, String param2) {
-        BusquedaFragment fragment = new BusquedaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busqueda, container, false);
+        View v = inflater.inflate(R.layout.fragment_busqueda, container,false);
+        btnBuscar = v.findViewById(R.id.btn_Buscar);
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listenerBuscar.buscar();//A buscar hay que pasarle un conjunto de info a buscar
+            }
+        });
+        return v;
     }
 }
--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -3,13 +3,19 @@ package com.mdgz.dam.labdam2022;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BusquedaFragment.OnBuscarListener{
+
+
 
     private Toolbar miToolbar;
 
@@ -30,11 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         switch (item.getItemId()){
             case R.id.mnuOptBuscar:
-                /* Agregar el intento del fragmento */
+                try {
+                    NavHostFragment navHostFragment =
+                            (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+                    NavController navController = navHostFragment.getNavController();
+
+                    navController.navigate(R.id.busquedaFragment);//Modificar para usar acciones????
+                }catch (Exception e){
+                }
                 return true;
+
             case R.id.mnuOptReservas:
                 /* Agregar el intento del fragmento */
                 return true;
@@ -48,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void buscar() {
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+
+        navController.navigate(R.id.action_busquedaFragment_to_resultadoBusquedaFragment);
     }
 }
