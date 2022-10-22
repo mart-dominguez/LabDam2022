95354382168871394e8278d699741730eafd59c9
SrTomico <audaytomas@gmail.com>
2022-10-22 15:04:08 -0300

    binding sin terminar

--- app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
@@ -4,61 +4,122 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BusquedaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.slider.RangeSlider;
+import com.mdgz.dam.labdam2022.databinding.FragmentBusquedaBinding;
+import com.mdgz.dam.labdam2022.model.Ciudad;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class BusquedaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentBusquedaBinding binding;
+    Spinner sCiudad;
+    com.google.android.material.button.MaterialButtonToggleGroup btgTipo;
+    EditText etPersonas;
+    ToggleButton tbWifi;
+    com.google.android.material.slider.RangeSlider rsRangoPrecio;
+    Button bBuscar;
+    Button bLimpiar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    int cantidadP= -1;
+    double minRango= -1;
+    double maxRango = -1;
 
-    public BusquedaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BusquedaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BusquedaFragment newInstance(String param1, String param2) {
-        BusquedaFragment fragment = new BusquedaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busqueda, container, false);
+        View view;
+        binding = FragmentBusquedaBinding.inflate(inflater, container, false);
+        tbWifi= binding.wifiTb;
+        sCiudad= binding.sCiudad;
+        btgTipo= binding.grupo;
+        etPersonas= binding.cantpEdit;
+        tbWifi= binding.wifiTb;
+        rsRangoPrecio= binding.rangoPrecioRs;
+        bBuscar= binding.buscarButton;
+        bLimpiar= binding.limpiarButton;
+        // meter todo en una funcion
+        ArrayList<Ciudad> ciudades = new ArrayList<Ciudad>();
+        ciudades.add(new Ciudad (1, "Santa Fe", "sfe"));
+        ciudades.add(new Ciudad (2, "Rosario", "sfe"));
+        ciudades.add(new Ciudad (3, "Buenos Aires", "sfe"));
+        ArrayAdapter<Ciudad> adapter =new ArrayAdapter <Ciudad> (getContext(), android.R.layout.simple_spinner_dropdown_item, ciudades);
+        sCiudad.setAdapter(adapter);
+        view = binding.getRoot();
+        return view;
     }
+    binding.tbWifi.setOnClickListener(){};
+
+
+
+//    private class InputValuesTextWatcher implements TextWatcher {
+//        @Override
+//        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//        }
+//
+//        @Override
+//        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//            if(CheckValidValuesOnInput()){
+//                binding.buscarButton.setEnabled(true);
+//            }
+//            else{
+//                binding.busquedaSpinner.setEnabled(false);
+//            }
+//        }
+//
+//
+//
+//        @Override
+//        public void afterTextChanged(Editable editable) {
+//
+//        }
+//    }
+//    protected Boolean CheckValidValuesOnInput(){
+//        try {
+//            cantidadP= Integer.parseInt(binding.cantpEdit.getText().toString());
+//
+//        }catch (Exception e){
+//            cantidadP = -1;
+//            return false;
+//        }
+//        try {
+//            tasaEfecAnual = Double.parseDouble(binding.tasaEfectivaAnualEditText.getText().toString());
+//        }catch (Exception e){
+//            tasaEfecAnual = -1;
+//            return false;
+//        }
+//        try {
+//            capitalInvertir = Double.parseDouble(binding.capitalInvertirEditText.getText().toString());
+//        }catch (Exception e){
+//            capitalInvertir = -1;
+//            return false;
+//        }
+//
+//
+//        if(tasaNomAnual <= 0 || tasaEfecAnual<= 0 || capitalInvertir <= 0){
+//            tasaNomAnual = -1;
+//            tasaEfecAnual = -1;
+//            capitalInvertir = -1;
+//            return false;
+//        }
+//
+//        return true;
+//    }
+
 }
