c2bba32e506f9ddf7a079590abb22540d455d239
MateMurcielago <galaxyuslol@gmail.com>
2022-11-04 19:33:15 -0300

    Actualización 4/11/2022

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -3,6 +3,10 @@ package com.mdgz.dam.labdam2022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar t1 = (Toolbar) findViewById(R.id.materialToolbar);
+        setSupportActionBar(t1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item_fav:
+                //Hacer algo cuando presionen Home
+                break;
+            case R.id.item_reservs:
+
+            default:
+                //Hacer algo cuando por default
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
