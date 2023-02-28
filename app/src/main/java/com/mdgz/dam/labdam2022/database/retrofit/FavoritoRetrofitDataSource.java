3fed74fb3ff8b6fe03d60378d93d5d1d410e878a
Tomas Assenza <tomyassenza@gmail.com>
2023-02-28 01:41:22 -0300

    Agregado retrofit y habitacionRest

---
+++ app/src/main/java/com/mdgz/dam/labdam2022/database/retrofit/FavoritoRetrofitDataSource.java
@@ -0,0 +1,17 @@
+package com.mdgz.dam.labdam2022.database.retrofit;
+
+import com.mdgz.dam.labdam2022.database.AlojamientoDataSource;
+import com.mdgz.dam.labdam2022.database.FavoritoDataSource;
+import com.mdgz.dam.labdam2022.model.Favorito;
+
+public class FavoritoRetrofitDataSource implements FavoritoDataSource {
+    @Override
+    public void guardarFavorito(Favorito entidad, GuardarFavoritoCallback callback) {
+
+    }
+
+    @Override
+    public void recuperarFavorito(RecuperarFavoritoCallback callback) {
+
+    }
+}
---
+++ app/src/main/java/com/mdgz/dam/labdam2022/database/retrofit/ReservaRetrofitDataSource.java
@@ -0,0 +1,17 @@
+package com.mdgz.dam.labdam2022.database.retrofit;
+
+import com.mdgz.dam.labdam2022.database.AlojamientoDataSource;
+import com.mdgz.dam.labdam2022.database.ReservaDataSource;
+import com.mdgz.dam.labdam2022.model.Reserva;
+
+public class ReservaRetrofitDataSource implements ReservaDataSource {
+    @Override
+    public void guardarReserva(Reserva entidad, GuardarReservaCallback callback) {
+
+    }
+
+    @Override
+    public void recuperarReserva(RecuperarReservaCallback callback) {
+
+    }
+}
